4d9ae888400c7e955a723364965fdc6818f40029
Andrew S Erwin <andrew@erwininteractive.com>
2023-01-31 14:35:22 -0600

    formatting in python

--- Python/sudokuSolver.py
+++ Python/sudokuSolver.py
@@ -14,36 +14,38 @@ puzzle = [[3,0,0,0,0,0,9,1,0],
           [6,0,0,8,0,0,4,0,0],
           [0,2,5,0,0,0,0,0,1]]
 
-def isPossible(x,y,n) :
+
+def isPossible(x, y, n):
     global puzzle
 
-    for i in range(9) :
-        if puzzle[y][i] == n :
+    for i in range(9):
+        if puzzle[y][i] == n:
             return False
-    
-    for i in range(9) :
-        if puzzle[i][x] == n :
+
+    for i in range(9):
+        if puzzle[i][x] == n:
             return False
-    
+
     x0 = (x//3)*3
     y0 = (y//3)*3
-    
-    for i in range(3) :
-        for j in range(3) :
-            if puzzle[y0+i][x0+j] == n :
+
+    for i in range(3):
+        for j in range(3):
+            if puzzle[y0+i][x0+j] == n:
                 return False
-    
+
     return True
 
-def solve() :
+
+def solve():
     global puzzle
     global iteration
-    
-    for y in range(9) :
-        for x in range(9) :
-            if puzzle[y][x] == 0 :
-                for n in range(1,10) :
-                    if isPossible(x,y,n) :
+
+    for y in range(9):
+        for x in range(9):
+            if puzzle[y][x] == 0:
+                for n in range(1, 10):
+                    if isPossible(x, y, n):
                         # Seems to work, let set it and keep going!
                         puzzle[y][x] = n
 
@@ -53,7 +55,8 @@ def solve() :
                         # It didn't work! Putting it back to 0
                         puzzle[y][x] = 0
                 return
-    
+
     print(np.matrix(puzzle))
+
 
 solve()
