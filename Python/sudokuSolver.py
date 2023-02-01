2c6eb1c8c54a15798d7d5f90aaacdfbb84947ccc
Andrew S Erwin <andrew@erwininteractive.com>
2023-02-01 01:52:44 -0600

    some formatting of the python sudoku solver

--- Python/sudokuSolver.py
+++ Python/sudokuSolver.py
@@ -1,22 +1,29 @@
+"""
+author: Andrew S Erwin
+link: https://github.com/erwininteractive
+
+Backtracking algorithm to solve sudoku puzzles
+"""
 import numpy as np
 
-# This is from a Computerphile video. Professor Thorsten Altenkirch
-# shows this method in Jupyter. It requires numpy
-# Original video - https://www.youtube.com/watch?v=G_UYXzGuqvM
-
-puzzle = [[3,0,0,0,0,0,9,1,0],
-          [0,0,9,0,0,3,0,0,8],
-          [0,4,0,0,0,2,0,0,0],
-          [0,6,0,0,5,0,0,9,7],
-          [0,0,7,2,0,9,3,0,0],
-          [9,3,0,0,7,0,0,5,0],
-          [0,0,0,5,0,0,0,7,0],
-          [6,0,0,8,0,0,4,0,0],
-          [0,2,5,0,0,0,0,0,1]]
+puzzle = [
+    [0, 0, 9, 0, 3, 0, 1, 0, 0],
+    [4, 0, 0, 1, 0, 0, 0, 9, 0],
+    [0, 5, 0, 0, 0, 4, 6, 0, 0],
+    [0, 0, 0, 7, 0, 3, 5, 0, 0],
+    [0, 8, 0, 0, 0, 0, 0, 2, 0],
+    [0, 0, 1, 4, 0, 5, 0, 0, 0],
+    [0, 0, 7, 2, 0, 0, 0, 5, 0],
+    [0, 2, 0, 0, 0, 8, 0, 0, 7],
+    [0, 0, 5, 0, 1, 0, 2, 0, 0]
+]
 
 
 def isPossible(x, y, n):
     global puzzle
+
+    x0 = (x//3)*3
+    y0 = (y//3)*3
 
     for i in range(9):
         if puzzle[y][i] == n:
@@ -25,9 +32,6 @@ def isPossible(x, y, n):
     for i in range(9):
         if puzzle[i][x] == n:
             return False
-
-    x0 = (x//3)*3
-    y0 = (y//3)*3
 
     for i in range(3):
         for j in range(3):
@@ -39,17 +43,16 @@ def isPossible(x, y, n):
 
 def solve():
     global puzzle
-    global iteration
 
     for y in range(9):
         for x in range(9):
             if puzzle[y][x] == 0:
                 for n in range(1, 10):
                     if isPossible(x, y, n):
-                        # Seems to work, let set it and keep going!
+                        # Try n
                         puzzle[y][x] = n
 
-                        # KEEP GOING
+                        # solve to test n
                         solve()
 
                         # It didn't work! Putting it back to 0
